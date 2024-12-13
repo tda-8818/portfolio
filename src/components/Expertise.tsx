7630aa2ca12b4e6c9532ba53d08056458a378f50
tda-8818 <ejj_t@outlook.com>
2024-12-13 19:56:35 +0800

    edited icons, some descriptions and tech stack

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faGamepad, faLayerGroup, faWifi } from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
@@ -16,27 +16,23 @@ const labelsFirst = [
     "Python",
     "SQL",
     "PostgreSQL",
-    "Postman"
+    "MongoDB"
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "Java",
+    "C#",
+    "C/C++",
+    ".NET",
+    "Springboot"
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
+    "React",
+    "TypeScript",
+    "JavaScript",
+    "Angular",
+    "",
     "LlamaIndex",
     "Streamlit",
 ];
@@ -45,12 +41,12 @@ function Expertise() {
     return (
     <div className="container" id="expertise">
         <div className="skills-container">
-            <h1>Expertise</h1>
+            <h1>Skills</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <FontAwesomeIcon icon={faLayerGroup} size="3x"/>
+                    <h3>Full Stack Development</h3>
+                    <p>I have built several desktop applications from scratch using .NET. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -60,9 +56,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faGamepad} size="3x"/>
+                    <h3>Game Development</h3>
+                    <p>I have built a diverse array of game applications. I have a strong proficiency in the MVC architecture, Abstract Factory pattern, SOLID principles, and core OOP concepts such as abstraction, encapsulation, inheritance and polymorphism.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -72,8 +68,8 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
+                    <FontAwesomeIcon icon={faWifi} size="3x"/>
+                    <h3>Web Development</h3>
                     <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
